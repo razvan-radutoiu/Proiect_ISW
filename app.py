cca85da4c81fb2c4b7ea435ecd6690b89e447dcd
razvan-radutoiu <72277109+razvan-radutoiu@users.noreply.github.com>
2024-02-25 13:44:28 +0200

    added basic features

--- app.py
+++ app.py
@@ -1,37 +1,66 @@
+from datetime import timedelta
 import db
 import json
-from flask import Flask
+from flask import Flask, request, render_template, jsonify, redirect, url_for, session
 
 app = Flask(__name__)
 app.secret_key = '1234'
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    pass
+    return render_template("signin.html")
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    pass
+    return render_template("signup.html")
 
 @app.route('/signin', methods=['POST'])
 def signin():
-    pass
+    status, username = db.check_user()
+    remember_me = request.form.get('remember-me')
+
+    data = {
+        "username": username,
+        "status": status
+    }
+
+    if status:
+        session['logged_in'] = True
+        session['username'] = username
+
+        if remember_me:
+            app.permanent_session_lifetime = timedelta(days=7)
+            session.permanent = True
+    else:
+        session.pop('logged_in', None)
+        session.pop('username', None)
+
+    return json.dumps(data)
 
 @app.route('/logout', methods=['POST'])
 def logout():
-    pass
+    session.pop('logged_in', None)
+    session.pop('username', None)
+    return redirect(url_for('home'))
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    pass
+    status = db.insert_user()
+    return json.dumps(status)
 
-@app.route('/menu')
+@app.route('/menu', methods=['GET', 'POST'])
 def menu():
-    pass
+    menu_data = db.get_menu()
+    if not session.get('logged_in'):
+        return redirect(url_for('home'))
+    else:
+        return render_template('menu.html', menu_data=menu_data)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-    pass
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
--- db.py
+++ db.py
@@ -5,24 +5,77 @@ import bcrypt
 client = pymongo.MongoClient('mongodb://127.0.0.1:27017/')
 
 db = client['university_cafeteria']  # DB name
-
 users = db['users']
 menu_items = db['menu_items']
 
 def hash_pass(password):
-    
+    """
+    Hashes a password using bcrypt.
+    """
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
 
-def insert_data():
-    pass
+def insert_user():
+
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['pass']
+
+
+        hashed_password = hash_pass(password)
+
+        reg_user = {
+            'name': name,
+            'email': email,
+            'password': hashed_password,
+            'role': 'customer'  # user role
+        }
+
+
+        if users.find_one({"email":email}) is None:
+            users.insert_one(reg_user)
+            return True
+        else:
+            return False, "User already in database"
+
 
 def check_user():
-    pass
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['pass']
+
+        user = {
+            "email": email
+        }
+
+        user_data = users.find_one(user)
+        if user_data is None:
+            return False, ""
+        else:
+            if bcrypt.checkpw(password.encode('utf-8'), user_data["password"]):
+                return True, user_data["name"]
+            else:
+                return False, ""
 
 def insert_menu_item():
-    pass
+    if request.method == 'POST':
+
+        name = request.form['name']
+        description = request.form['description']
+        price = float(request.form['price'])
+        image_url = request.form['image_url']
+
+        menu_item = {
+            "name": name,
+            "description": description,
+            "price": price,
+            "image_url": image_url
+        }
+
+        menu_items.insert_one(menu_item)
+
 
 def get_menu():
-    pass
+    return menu_items.find({})
